575f2baad37991749a4236cba6148085a13c5d72
Fagner Sales <fs.fagnersales@gmail.com>
2021-04-02 11:52:35 -0300

    Added export for class Button

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import { DiscordUI } from './src/DiscordUI'
 import { Page } from './src/Page'
 import { Item } from './src/Item'
+import { Button } from './src/Button'
 
-export { DiscordUI, Page, Item }
+export { DiscordUI, Page, Item, Button }
